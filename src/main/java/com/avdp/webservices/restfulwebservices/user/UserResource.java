be00c7fb5f025b74a6b5d216c97827f754bf3b9d
vdepalma <vdepalma@nalandaglobal.com>
2020-10-09 15:19:07 +0200

    first commit

---
+++ src/main/java/com/avdp/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,45 @@
+package com.avdp.webservices.restfulwebservices.user;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserResource {
+
+	@Autowired
+	private UserDaoService service;
+
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+
+	}
+
+	@GetMapping("/users/{id}")
+	public User retrieveOneUsers(@PathVariable int id) {
+		User user = service.findOne(id);
+		if (user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+		return user;
+
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<Object> saveUser(@RequestBody User user) {
+		User saved = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(saved.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
+}
